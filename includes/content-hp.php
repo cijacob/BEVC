a3d55d635bfcee91514838005dd353f37affe7e1
Cindy Jacob <jacobcindy@live.be>
2020-02-28 12:05:35 +0100

    Add private file

--- includes/content-hp.php
+++ includes/content-hp.php
@@ -26,6 +26,20 @@
 		<div class="title">
 			<h2>Ce weekend en photos!</h2>
 		</div>
+		<div class="hp__imgHero">
+			<div class="imgHero">
+				<img src="https://picsum.photos/800/400?grayscale" alt="" class="inner__imgHero">
+			</div>
+			<div class="imgHero">
+				<img src="https://picsum.photos/800/400?grayscale"  alt="" class="inner__imgHero">
+			</div>
+			<div class="imgHero">
+				<img src="https://picsum.photos/800/400?grayscale"  alt="" class="inner__imgHero">
+			</div>
+			<div class="imgHero">
+				<img src="https://picsum.photos/800/400?grayscale"  alt="" class="inner__imgHero">
+			</div>
+		</div>
 		<div class="hp__innerGallery">
 			<div class="gallery">
 				<img src="https://picsum.photos/200/300" alt="">
@@ -37,8 +51,6 @@
 				<img src="https://picsum.photos/300/300" alt="">
 				<img src="https://picsum.photos/200/300" alt="">
 				<img src="https://picsum.photos/500/300" alt="">
-				<img src="https://picsum.photos/300/300" alt="">
-				<img src="https://picsum.photos/200/300" alt="">
 			</div>
 			<div class="follow_us">
 				<p>Pour plus de photos et vous tenir informés, suivez-vous sur les réseaux sociaux!</p>
---
+++ infos/vie-privee.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<?php $location = '../'?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>BEVC | Vie privée</title>
+	<?php include  $location . 'includes/header.php'; ?>
+</head>
+<body>
+	<header>
+		<?php include $location . 'includes/menu.php';  ?>
+		<div class="pic_presentation img_bg_academie">
+			<div class="inner_pic_presentation">
+				<h3 class="title_header_block">Académie</h3>
+			</div>
+		</div>
+	</header>
+	<main>
+		<div class="container_personnal_content">
+      <div class="title">
+				<h2>Vie privée</h2>
+      </div>
+      <div class="intro">
+				<p>
+          Conformément à la loi du 8 décembre 1992 relative à la protection de la vie privée à l’égard des traitements de données à caractère personnel, 
+          les données à caractère personnel que vous nous communiquez sont enregistrées par Bruxelles Est volley Club qui est le maître du fichier. 
+          Elles seront uniquement utilisées afin d’assurer le suivi de votre affiliation.
+          En vertu de la loi du 11 décembre 1998 (A.R 13.02.2001), vous bénéficiez d’un droit d’accès et de rectification de vos données personnelles en notre possession. 
+          Celui-ci peut utilement être exercé sur demande écrite à Bruxelles est Volley Club.
+        </p>
+			</div>
+		</div>
+	</main>
+	<footer>
+    <?php include $location . 'includes/footer.php'; ?>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>  
+</body>
+</html>
